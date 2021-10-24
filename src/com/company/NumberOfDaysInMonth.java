5232e7d402fa72e5c2c02798a54c47891672320c
Eugeniusz W <eugwas@gmail.com>
2021-10-24 16:49:56 +0200

    First commit

---
+++ src/com/company/NumberOfDaysInMonth.java
@@ -0,0 +1,25 @@
+package com.company;
+
+public class NumberOfDaysInMonth {
+    public static boolean isLeapYear(int year) {
+        if (year < 1 || year > 9999)
+            return false;
+        else if (year % 4 != 0)
+            return false;
+        else if (year % 100 == 0 && year % 400 != 0)
+            return false;
+        return true;
+    }
+
+    public static int getDaysInMonth(int month, int year) {
+        if (month < 1 || month > 12 || year < 1 || year > 9999)
+            return -1;
+        else if (month == 4 || month == 6 || month == 9 || month == 11)
+            return 30;
+        else if (month == 2 && isLeapYear(year) == true)
+            return 29;
+        else if (month == 2 && isLeapYear(year) == false)
+            return 28;
+        return 31;
+    }
+}
